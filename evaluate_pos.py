980f5bcc3988952347f20c216a72154c1b39f8fc
Shawn Zhong <github@shawnzhong.com>
2020-02-21 07:37:35 -0600

    Allow mocking BOS

--- evaluate_pos.py
+++ evaluate_pos.py
@@ -8,26 +8,31 @@ from datasets.word_freq.polyglot import get_polyglot_frequency_path
 languages = ['kk', 'ta', 'lv', 'vi', 'hu', 'tr', 'el', 'bg', 'sv', 'eu', 'ru', 'da', 'id', 'zh', 'fa', 'he', 'ro',
              'en', 'ar', 'hi', 'it', 'es', 'cs']
 
-for language_code in ['en']:
+def evaluate(language_code, mock_bos=False):
     target_embeddings_path = get_polyglot_embeddings_path(language_code)
     word_frequency_path = get_polyglot_frequency_path(language_code)
-    result_path = f'./results/pbos/polyglot/{language_code}'
+    result_path = f'./results/pbos/polyglot/{language_code}' if not mock_bos else f'./results/bos/polyglot/{language_code}'
     subword_embedding_model_path = result_path + '/model.pbos'
     training_log_path = subword_embedding_model_path + ".log"
     os.makedirs(result_path, exist_ok=True)
 
     if not os.path.exists(subword_embedding_model_path):
         with open(training_log_path, 'w+') as log:
-            p = sp.Popen(f"""
+            command = f"""
             python pbos_train.py \
               --target_vectors {target_embeddings_path} \
               --model_path {subword_embedding_model_path} \
               --word_list {word_frequency_path} \
               --word_list_has_freq \
               --boundary \
-              --word_list_size 1000000\
-              --sub_min_len 3
-            """.split(), stdout=log, stderr=log)
+              --word_list_size 1000000 \
+              --sub_min_len 3 
+            """
+
+            if mock_bos:
+                command += " --mock_bos"
+
+            p = sp.Popen(command.split(), stdout=log, stderr=log)
             p.wait()
 
     ud_data_path, ud_vocab_path = get_universal_dependencies_path(language_code)
@@ -50,3 +55,9 @@ for language_code in ['en']:
       --dropout 0.5 \
       --no-we-update 
     """.split())
+
+
+for language_code in ['en']:
+    evaluate(language_code, mock_bos=False)
+    evaluate(language_code, mock_bos=True)
+
