898b610e4e33760441d5b97f28f26e6ab1847d6e
Shawn Zhong <github@shawnzhong.com>
2020-04-09 04:55:53 -0500

    Update trainer driver

--- compact_reconstruction.py
+++ compact_reconstruction.py
@@ -3,48 +3,50 @@ import pickle
 import subprocess as sp
 import multiprocessing as mp
 
-emb_dir = "datasets/polyglot_embeddings"
-freq_dir = "datasets/polyglot_freq"
-result_dir = "results/compact_reconstruction"
-
-langs = [filename[:-4] for filename in os.listdir(emb_dir) if filename.endswith(".pkl")]
+from datasets.polyglot_embeddings import get_polyglot_embeddings_path, get_polyglot_codecs_path, languages
+from datasets.polyglot_freq import get_polyglot_frequency_path
 
 
-def convert_pkl_to_word2vec_format(lang):
-    with open(f"{emb_dir}/{lang}.pkl", "rb") as fin:
-        with open(f"{emb_dir}/{lang}.txt", "w") as fout:
-            vocab, emb = pickle.load(fin, encoding="bytes")
-            print(len(vocab), len(emb[0]), file=fout)
-            for v, e in zip(vocab, emb):
-                print(v, *e, file=fout)
-
-
-def train(lang):
-    convert_pkl_to_word2vec_format(lang)
+def train(emb_path, freq_path, codecs_path, result_path, use_hash=False):
+    cmd = f"""
+        python compact_reconstruction/src/train.py 
+            --gpu 0 
+            --ref_vec_path {emb_path} 
+            --freq_path {freq_path or ""} 
+            --codecs_path {codecs_path or ""}
+            --embed_dim 64 
+            --maxlen 200 
+            --network_type 3 
+            --subword_type {4 if use_hash else 0}
+            --limit_size 100000 
+            --result_dir {result_path} 
+            --unique_false 
+        """
+    
+    if use_hash:
+        cmd += """
+            --multi_hash two
+            --hashed_idx
+            --bucket_size 100000 
+        """
+    
     sp.call(
-        f"""
-        python compact_reconstruction/src/train.py \
-            --gpu 0 \
-            --ref_vec_path {emb_dir}/{lang}.txt \
-            --freq_path {freq_dir}/{lang}.txt \
-            --multi_hash two \
-            --embed_dim 64 \
-            --maxlen 200 \
-            --network_type 3 \
-            --subword_type 4 \
-            --limit_size 1000000 \
-            --bucket_size 100000 \
-            --result_dir {result_dir}/{lang} \
-            --hashed_idx \
-            --unique_false
-        """.split(),
+        cmd.split(),
         env={**os.environ, "CUDA_PATH": "/usr/local/cuda-10.2"},
     )
 
 
 with mp.Pool() as pool:
-    for lang in langs:
-        pool.apply_async(train, (lang,))
+    for lang in languages[:1]:
+        # input
+        emb_path = get_polyglot_embeddings_path(lang).w2v_path
+        freq_path = get_polyglot_frequency_path(lang).raw_count_path
+        codecs_path = get_polyglot_codecs_path(lang)
+
+        # output
+        result_path = f"results/compact_reconstruction/polyglot_KVQ_F/{lang}"
+
+        pool.apply_async(train, (emb_path, freq_path, codecs_path, result_path))
 
     pool.close()
     pool.join()
