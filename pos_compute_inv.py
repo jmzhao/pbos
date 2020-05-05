8ec0dbbc28386b8a5a9620865b71d320a728f1b0
Shawn Zhong <github@shawnzhong.com>
2020-05-05 02:49:14 -0500

    Update inv script

--- pos_compute_inv.py
+++ pos_compute_inv.py
@@ -6,14 +6,14 @@ from load import load_embedding
 from datasets.polyglot_embeddings import get_polyglot_embeddings_path, languages
 from datasets.universal_dependencies import get_universal_dependencies_path
 
-languages = ["cs"]
 for language in languages:
 
     polyglot_path = get_polyglot_embeddings_path(language).pkl_path
     polyglot_vocab, _ = load_embedding(polyglot_path)
+    polyglot_vocab = set(polyglot_vocab)
     
     _, ud_vocab_path =  get_universal_dependencies_path(language)
     ud_vocab = [l.strip() for l in open(ud_vocab_path)]
         
-    oov = sum(w in polyglot_vocab for w in ud_vocab) / len(ud_vocab)
-    print(language, oov)
+    inv = sum(w in polyglot_vocab for w in ud_vocab) / len(ud_vocab)
+    print(language, inv)
