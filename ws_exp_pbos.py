9013747bf09d725481fea55e2b44db14e68a6f18
Shawn Zhong <github@shawnzhong.com>
2020-05-07 15:35:53 -0500

    Use mp

--- ws_exp_pbos.py
+++ ws_exp_pbos.py
@@ -1,18 +1,26 @@
 import os
 import subprocess as sp
-import sys
+import multiprocessing as mp
 
-for model_type in ['bos', 'pbos']:
-    for target_vectors in ["google_news", "polyglot"]:
-        results_dir = f"results/ws_{target_vectors}_{model_type}"
-        os.makedirs(results_dir, exist_ok=True)
-        log_path = os.path.join(results_dir, "demo.log")
-        model_path = os.path.join(results_dir, "model.pbos")
-        cmd = f"python pbos_demo.py \
+
+def exp(model_type, target_vectors):
+    results_dir = f"results/ws_{target_vectors}_{model_type}"
+    os.makedirs(results_dir, exist_ok=True)
+    log_path = os.path.join(results_dir, "demo.log")
+    model_path = os.path.join(results_dir, "model.pbos")
+    cmd = f"python pbos_demo.py \
             --model_path {model_path} \
             --model_type {model_type} \
             --target_vectors {target_vectors} \
         ".split()
-        with sp.Popen(['/usr/bin/tee', '-a', log_path], stdin=sp.PIPE) as tee:
-            sp.call(cmd, stderr=tee.stdin)
-        # sp.call(cmd)
+    with sp.Popen(['/usr/bin/tee', '-a', log_path], stdin=sp.PIPE) as tee:
+        sp.call(cmd, stderr=tee.stdin)
+
+
+with mp.Pool() as pool:
+    results = [pool.apply_async(exp, (model_type, target_vectors))
+               for model_type in ['bos', 'pbos']
+               for target_vectors in ["google_news", "polyglot"]]
+
+    for r in results:
+        r.get()
