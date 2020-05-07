c5dfa1ba83c970063f5f88aecb763a05ccbd000d
Jinman Zhao <jinman.zhao@gmail.com>
2020-05-07 12:36:08 -0500

    affix prediction

---
+++ affix_eval.py
@@ -0,0 +1,93 @@
+import argparse
+import importlib
+from itertools import islice
+import logging
+import pickle
+from collections import namedtuple
+from itertools import chain, repeat
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import sklearn.metrics
+from tqdm import tqdm
+
+from load import load_embedding
+from utils.args import add_logging_args, logging_config
+
+parser = argparse.ArgumentParser("Evaluate embedding on affix prediction.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--dataset', default="affix",
+    choices=["affix"],
+    help="path to affix prediction dataset")
+parser.add_argument('--embeddings',
+    help="path to word embeddings")
+parser.add_argument('--random_seed', type=int, default=42,
+    help="random seed for training the classifier")
+add_logging_args(parser)
+args = parser.parse_args()
+
+logging_config(args)
+
+
+Instance = namedtuple("Instance", "word affix")
+
+
+## Load affix prediction data
+get_affix_path = getattr(
+    importlib.import_module(f"datasets.{args.dataset}"),
+    f"get_{args.dataset}_path",
+)
+affix_raw_path = get_affix_path().raw_path
+dataset = {"test": [], "train": []}
+with open(affix_raw_path) as fin:
+    for line in islice(fin, 1, None): ## skip the title row
+        ## row fmt: affix	stem	stemPOS	derived	derivedPOS	type	...
+        affix, stem, _, derived, _, split = line.split()[:6]
+        dataset[split].append(Instance(word = derived, affix = affix))
+all_affixes = set(ins.affix for ins in dataset["train"])
+affixes_a2i = {a : i for i, a in enumerate(sorted(all_affixes))}
+from collections import Counter
+print(Counter(ins.affix for ins in dataset["test"]))
+
+
+## Load embeddings
+vocab, emb = load_embedding(args.embeddings)
+emb_w2i = {w : i for i, w in enumerate(vocab)}
+
+## Prepare training and testing data arrays
+def make_X(instance):
+    X = emb[emb_w2i[instance.word]]
+    return X
+
+def make_y(instance):
+    return np.array(affixes_a2i[instance.affix])
+
+def make_X_y(instances):
+    X = np.stack([make_X(ins) for ins in tqdm(instances)])
+    logging.info(f"X.shape = {X.shape}")
+    y = np.stack([make_y(ins) for ins in instances])
+    logging.info(f"y.shape = {y.shape}")
+    return X, y
+
+logging.info("building training instances...")
+train_X, train_y = make_X_y(dataset["train"])
+logging.info("building test instances...")
+test_X,  test_y  = make_X_y(dataset["test"])
+
+## Train a logistic regression classifier and report scores
+logging.info("training...")
+clsfr = LogisticRegression(random_state=args.random_seed, verbose=False)
+clsfr.fit(train_X, train_y)
+# print("Train acc: {}".format(clsfr.score(train_X, train_y)))
+print("Test acc:  {}".format(clsfr.score( test_X,  test_y)))
+pred_y = clsfr.predict(test_X)
+for average_scheme in ('micro', 'macro'):
+    for score_name in ('precision', 'recall', 'f1'):
+        print("{} {}:\t{}".format(
+            average_scheme,
+            score_name,
+            getattr(sklearn.metrics, score_name + "_score")(
+                test_y, pred_y,
+                average=average_scheme,
+            ),
+        ))
---
+++ datasets/affix/__init__.py
@@ -0,0 +1,55 @@
+import gzip
+from itertools import islice
+import json
+import logging
+import os
+from pathlib import Path
+import shutil
+import subprocess as sp
+
+from utils import dotdict
+
+
+logger = logging.getLogger(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_affix_path(
+    *,
+    dir_path=dir_path,
+):
+    dir_path = Path(dir_path)
+    gz_path = dir_path / "affix_complete_set.txt.gz"
+    raw_path = dir_path / "affix_complete_set.txt"
+    queries_path = dir_path / "queries.txt"
+
+    if not os.path.exists(gz_path):
+        logger.info(f"Downloading {gz_path}")
+        url = "http://marcobaroni.org/PublicData/affix_complete_set.txt.gz"
+        sp.run(f"wget -O {gz_path} {url}".split())
+
+    if not os.path.exists(raw_path):
+        logger.info(f"Unzipping {raw_path}")
+        with gzip.open(gz_path, 'rb') as fin, open(raw_path, 'wb') as fout:
+            shutil.copyfileobj(fin, fout)
+
+    if not os.path.exists(queries_path):
+        logger.info(f"Making {queries_path}")
+        with open(raw_path) as fin, open(queries_path, 'w') as fout:
+            for line in islice(fin, 1, None): ## skip the title row
+                ## row fmt: affix	stem	stemPOS	derived	derivedPOS	type	...
+                affix, stem, _, derived, _, split = line.split()[:6]
+                print(derived, file=fout)
+
+    return dotdict(
+        dir_path = dir_path,
+        gz_path = gz_path,
+        raw_path = raw_path,
+        queries_path = queries_path,
+    )
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.info)
+    get_affix_path()
