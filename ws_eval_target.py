6e8cbe049ac3ecb144c5c3bd1561e9ac9e9f9d5c
Shawn Zhong <github@shawnzhong.com>
2020-05-07 12:25:03 -0500

    Update ws_eval_target.py

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -1,18 +1,18 @@
-import subprocess as sp
 from pathlib import Path
+
+from datasets.ws_bench import BENCHS, prepare_bench_paths
 from ws_eval import eval_ws
 
 from datasets.google import prepare_google_paths
 from datasets.polyglot_emb import prepare_polyglot_emb_paths
 
 models = {
-    "google news": prepare_google_paths().txt_emb_path,
     "ployglot": prepare_polyglot_emb_paths("en").txt_emb_path,
+    "google": prepare_google_paths().txt_emb_path,
 }
 
 for model_name, model_path in models.items():
-    for dataset in ("wordsim353", "rw", "card660"):
-        data_path = Path(".") / "datasets" / dataset / f"{dataset}.txt"
-        # result = eval_ws(model_path, data_path, lower=True)
+    for dataset in BENCHS:
+        data_path = prepare_bench_paths(dataset).txt_path
         result = eval_ws(model_path, data_path, lower=True, oov_handling="zero")
         print(model_name, result)
