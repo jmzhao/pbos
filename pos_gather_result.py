54863cecf818eca416e3ff2689a87fda97de5397
Shawn Zhong <github@shawnzhong.com>
2020-05-08 07:25:33 -0500

    Update pos_gather_result.py

--- pos_gather_result.py
+++ pos_gather_result.py
@@ -4,15 +4,17 @@ A simple script to gather the result for POS
 
 from pathlib import Path
 
-pos_result_dir = Path(".") / "results" / "polyglot"
+pos_result_dir = Path("results") / "polyglot"
+
 
 def get_acc(lang, model_type):
-    out_path = lang / model_type/ "ud.out"
+    out_path = lang / model_type / "ud.out"
     with open(out_path, 'r') as f:
         lines = f.read().splitlines()
         last_line = lines[-1]
         _, acc = last_line.split(":")
         return acc.strip()
+
 
 for lang in sorted(pos_result_dir.iterdir()):
     print(lang.name, *(get_acc(lang, m) for m in ("bos", "pbos")), sep="\t")
