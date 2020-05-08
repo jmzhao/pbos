a133d78b38ee4226115a78d0fa8ed8b7ef59e008
Shawn Zhong <github@shawnzhong.com>
2020-05-08 09:21:37 -0500

    Move some logic to subwords.py

--- ws_exp_pbos.py
+++ ws_exp_pbos.py
@@ -3,10 +3,12 @@ import subprocess as sp
 import multiprocessing as mp
 
 
-def exp(model_type, target_vectors):
+def exp(model_type, target_vectors, subword_prob_eps=None):
     results_dir = f"results/ws_{target_vectors}_{model_type}"
+    if subword_prob_eps:
+        results_dir += f"_eps={subword_prob_eps}"
     os.makedirs(results_dir, exist_ok=True)
-    log_path = os.path.join(results_dir, "demo.log")
+
     model_path = os.path.join(results_dir, "model.pbos")
     cmd = f"python pbos_demo.py \
             --model_path {model_path} \
@@ -17,15 +19,20 @@ def exp(model_type, target_vectors):
     if model_type == 'bos':
         cmd += "--subword_min_len 3 " \
                "--subword_max_len 6"
+    else:
+        cmd += f"--subword_prob_eps {subword_prob_eps}"
+
+    log_path = os.path.join(results_dir, "demo.log")
     with sp.Popen(['/usr/bin/tee', '-a', log_path], stdin=sp.PIPE) as tee:
         sp.call(cmd.split(), stderr=tee.stdin)
 
 
 with mp.Pool() as pool:
-    results = [
-        pool.apply_async(exp, (model_type, target_vectors))
-        for model_type in ['bos', 'pbos']
-        for target_vectors in ["google_news", "polyglot"]
-    ]
+    results = []
+    for target_vectors in ("polyglot", "google_news"):
+        for subword_prob_eps in (0.01, 0.1, 0.2, 0.3, 0.5):
+            results.append(pool.apply_async(exp, ('pbos', target_vectors, subword_prob_eps)))
+        results.append(pool.apply_async(exp, ('bos', target_vectors)))
+
     for r in results:
         r.get()
