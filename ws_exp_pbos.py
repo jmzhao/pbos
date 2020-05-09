2d9753058dcd31e02796e0e1151a829cde825564
Shawn Zhong <github@shawnzhong.com>
2020-05-09 08:37:37 -0500

    New `ws_exp_pbos` that does not rely on `pbos_demo.py`

--- ws_exp_pbos.py
+++ ws_exp_pbos.py
@@ -1,38 +1,120 @@
 import os
 import subprocess as sp
 import multiprocessing as mp
+import argparse
+from collections import ChainMap
+from datasets.google import prepare_google_paths
+from datasets.polyglot_emb import prepare_polyglot_emb_paths
+from datasets.unigram_freq import prepare_unigram_freq_paths
+import pbos_train
+import subwords
+from datasets.ws_bench import prepare_bench_paths, BENCHS, prepare_combined_query_path
+from utils import dotdict
+from utils.args import add_logging_args, set_logging_config, dump_args
+from ws_eval import eval_ws
 
 
-def exp(model_type, target_vectors, subword_prob_eps=None):
-    results_dir = f"results/ws_{target_vectors}_{model_type}"
-    if subword_prob_eps:
-        results_dir += f"_eps={subword_prob_eps}"
-    os.makedirs(results_dir, exist_ok=True)
+def train(args):
+    subwords.build_subword_vocab_cli(dotdict(ChainMap(
+        dict(
+            command='build_vocab',
+            output=args.subword_vocab
+        ), args,
+    )))
 
-    model_path = os.path.join(results_dir, "model.pbos")
-    cmd = f"python pbos_demo.py \
-            --model_path {model_path} \
-            --model_type {model_type} \
-            --target_vectors {target_vectors} \
-            --epochs 50 \
-        "
-    if model_type == 'bos':
-        cmd += "--subword_min_len 3 " \
-               "--subword_max_len 6"
-    else:
-        cmd += f"--subword_prob_eps {subword_prob_eps}"
+    if args.subword_prob:
+        subwords.build_subword_prob_cli(dotdict(ChainMap(
+            dict(
+                command='build_prob',
+                word_freq=prepare_unigram_freq_paths().word_freq_path,
+                output=args.subword_prob
+            ), args,
+        )))
 
-    log_path = os.path.join(results_dir, "demo.log")
-    with sp.Popen(['/usr/bin/tee', '-a', log_path], stdin=sp.PIPE) as tee:
-        sp.call(cmd.split(), stderr=tee.stdin)
+    pbos_train.main(args)
+
+
+def eval(args):
+    query_path = prepare_combined_query_path()
+    pred_path = f"{args.results_dir}/vectors.txt"
+    sp.call(f"""
+        python pbos_pred.py \
+          --queries {query_path} \
+          --save {pred_path} \
+          --model {args.model_path} \
+          {'--' if args.word_boundary else '--no_'}word_boundary \
+    """.split())
+
+    for bname in BENCHS:
+        bench_paths = prepare_bench_paths(bname)
+        for lower in (True, False):
+            result = eval_ws(pred_path, bench_paths.txt_path, lower=lower, oov_handling='zero')
+            with open(f"{args.results_dir}/ws_result.txt", "a+") as fout:
+                print(result, file=fout)
+
+
+def get_default_args():
+    parser = argparse.ArgumentParser()
+    add_logging_args(parser)
+    pbos_train.add_training_args(parser)
+    pbos_train.add_model_args(parser)
+    subwords.add_subword_args(parser)
+    subwords.add_subword_prob_args(parser)
+    subwords.add_subword_vocab_args(parser)
+
+    parser_action_lookup = {
+        action.dest: action
+        for action in parser._actions
+        # the following test is needed, otherwise `--no_<flag>` option will
+        # overwrite `--<flag>` option.
+        if not any(s.startswith('--no_') for s in action.option_strings)
+    }
+    parser_action_lookup["subword_vocab"].required = False
+
+    return dotdict(vars(parser.parse_args()))
+
+
+def get_target_vector_paths(target_vector_name):
+    if target_vector_name.lower() == "google_news":
+        return prepare_google_paths()
+    elif target_vector_name.lower() == "polyglot":
+        return prepare_polyglot_emb_paths("en")
+    raise NotImplementedError
+
+
+def exp(model_type, target_vector_name):
+    args = get_default_args()
+
+    # setup additional args
+    target_vector_paths = get_target_vector_paths(target_vector_name)
+    args.model_type = model_type
+    args.results_dir = os.path.join("results", "test")
+    args.model_path = os.path.join(args.results_dir, "model.pbos")
+    args.target_vectors = target_vector_paths.txt_emb_path
+    args.word_freq = target_vector_paths.word_freq_path  # will get overridden for prob
+    args.subword_vocab = os.path.join(args.results_dir, 'subword_vocab.jsonl')
+    args.subword_prob = os.path.join(args.results_dir, 'subword_prob.jsonl') if args.model_type == 'pbos' else None
+
+    set_logging_config(args)
+    dump_args(args)
+
+    os.makedirs(args.results_dir, exist_ok=True)
+
+    train(args)
+    eval(args)
 
 
 with mp.Pool() as pool:
-    results = []
-    for target_vectors in ("polyglot", "google_news"):
-        for subword_prob_eps in (0.01, 0.1, 0.2, 0.3, 0.5):
-            results.append(pool.apply_async(exp, ('pbos', target_vectors, subword_prob_eps)))
-        results.append(pool.apply_async(exp, ('bos', target_vectors)))
+    model_types = ('pbos',)
+    target_vector_names = ("polyglot",)
+    subword_prob_min_probs = (0,)
+    wbs = (True,)
+
+    results = [
+        pool.apply_async(exp, (model_type, target_vector_name))
+        for model_type in model_types
+        for target_vector_name in target_vector_names
+    ]
 
     for r in results:
         r.get()
