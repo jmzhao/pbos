2069b9acee0c6446c80362f1c2583a5a7e4ff4fa
Shawn Zhong <6421097+ShawnZhong@users.noreply.github.com>
2020-05-21 18:05:05 -0500

    Add GloVe pretrained support  (#7)
    * Fix renamed `ref_vec_name` in affix_exp.py
    
    * Add `subword_uniq_factor` to ws exp
    
    * Change subword_prob_min_prob in pos_exp
    
    * Explicitly set word_boundary in the exp script
    
    * Add affix to `prepare_combined_query_path`
    
    * Add affix_eval to `ws_exp_pbos`
    
    * Fix typo
    
    * Split evaluate and predict in exp script
    
    * Change print to log in `affix_eval`
    
    * Explicitly specify subword_prob_min_prob in ws_exp
    
    * Rename `ws_exp_pbos`
    
    * Remove `get_default_args`
    
    * Update ws_affix_exp_pbos.py
    
    * Add .vscode to gitignore
    
    * Remove wb & subword_prob_min_prob in pos_exp
    
    * Add `subword_prob_eps` in pos_exp
    
    * Revert "Add `subword_prob_eps` in pos_exp"
    
    This reverts commit 517e71090284d5880b9504871835a169569e443f.
    
    * Create __init__.py
    
    * Update load.py
    
    * Update ws_affix_exp_pbos.py
    
    * Update load.py
    
    * Update load.py
    
    * Update load.py
    
    * Update load.py
    
    * Update __init__.py
    
    * Update load.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update ws_eval_target.py
    
    * Update ws_eval_target.py

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -1,5 +1,4 @@
-from pathlib import Path
-
+from datasets.glove import prepare_glove_paths
 from datasets.ws_bench import BENCHS, prepare_bench_paths
 from ws_eval import eval_ws
 
@@ -10,10 +9,11 @@ models = {
     "EditSim": "EditSim",
     "ployglot": prepare_polyglot_emb_paths("en").txt_emb_path,
     "google": prepare_google_paths().txt_emb_path,
+    "glove": prepare_glove_paths().txt_emb_path,
 }
 
 for model_name, model_path in models.items():
     for dataset in BENCHS:
         data_path = prepare_bench_paths(dataset).txt_path
-        result = eval_ws(model_path, data_path, lower=False, oov_handling="zero")
+        result = eval_ws(model_path, data_path, lower=True, oov_handling="zero")
         print(model_name, result)
