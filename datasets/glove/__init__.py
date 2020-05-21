fc3fe6b0ccb90981a04a1931bb38e0ff73579466
Shawn Zhong <github@shawnzhong.com>
2020-05-21 08:18:12 -0500

    Update __init__.py

--- datasets/glove/__init__.py
+++ datasets/glove/__init__.py
@@ -4,8 +4,7 @@ import zipfile
 import subprocess as sp
 import os
 
-from utils import dotdict
-from load import load_embedding
+from utils import dotdict, file_tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +15,8 @@ txt_emb_path = f"{dir_path}/glove.840B.300d.processed.txt"
 w2v_path = f"{dir_path}/glove.840B.300d.processed.w2v"
 word_freq_path = f"{dir_path}/word_freq.jsonl"
 raw_count_path = f"{dir_path}/word_freq.txt"
+
+emb_dim = 300
 
 
 def prepare_glove_paths(
@@ -32,21 +33,32 @@ def prepare_glove_paths(
         url = "http://nlp.stanford.edu/data/glove.840B.300d.zip"
         sp.run(f"wget -O {zip_path} {url}".split())
 
-    # glove includes ~20 words with space which is hard to handle in current codebase
-    # need to clean them from `raw_emb`
     if not os.path.exists(raw_emb_path):
         logger.info("unzipping...")
         with zipfile.ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(dir_path)
 
-    if not os.path.exists(w2v_path) or not os.path.exists(txt_emb_path):
-        logger.info("generating emb file...")
-        vocab, emb = load_embedding(raw_emb_path)
-        with open(w2v_path, "w") as w2v_file, open(txt_emb_path, "w") as txt_emb_file:
-            print(len(vocab), len(emb[0]), file=w2v_file)
-            for v, e in zip(vocab, emb):
-                print(v, *e, file=txt_emb_file)
-                print(v, *e, file=w2v_file)
+    # glove includes ~20 words with space which is hard to handle in current codebase
+    # need to clean them from `raw_emb`
+    if not os.path.exists(txt_emb_path):
+        logger.info("generating txt emb file...")
+        with open(raw_emb_path, "r") as fin, open(txt_emb_path, "w") as fout:
+            vocab_len = 0
+            for line in file_tqdm(fin):
+                ss = line.split()
+                if len(ss) != emb_dim + 1:
+                    logging.critical(f'line "{line[:30]}"... might include word with space, skipped')
+                    continue
+
+                vocab_len += 1
+                fout.write(line)
+
+    if not os.path.exists(w2v_path):
+        logger.info("generating w2v emb file...")
+        with open(txt_emb_path) as fin, open(w2v_path, "w") as fout:
+            print(vocab_len, emb_dim, file=fout)
+            for line in file_tqdm(fin):
+                fout.write(line)
 
     if not os.path.exists(word_freq_path):
         logger.info("generating word freq jsonl file...")
