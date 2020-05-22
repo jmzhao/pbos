a3c17cbbe9773ba3253af824c1c33326eefdd308
Shawn Zhong <github@shawnzhong.com>
2020-05-21 23:18:50 -0500

    Update affix_search_reg.py

--- affix_search_reg.py
+++ affix_search_reg.py
@@ -4,18 +4,30 @@ Script used to tune regression C for affix
 import argparse
 import os
 import subprocess as sp
+import multiprocessing as mp
+from itertools import product
 
 
-def main(args):
-    results_dir = "results/affix"
+def evaluate(results_dir, embeddings, C):
+    with open(f"{results_dir}/affix_C={C}", "w+") as f:
+        sp.call(f"python affix_eval.py --embeddings {embeddings} --C {C}".split(), stdout=f)
+
+
+def main(results_dir, embeddings):
     os.makedirs(results_dir, exist_ok=True)
-    for C in [500, 600, 700, 800, 1000, 2000, 3000, 4000, 5000]:
-        with open(f"{results_dir}/affix_C={C}", "w+") as f:
-            sp.call(f"python affix_eval.py --embeddings {args.embeddings} --C {C}".split(), stdout=f)
+    with mp.Pool() as pool:
+        results = [
+            pool.apply_async(evaluate, (results_dir, embeddings, C,))
+            for C in sorted(x * 10 ** b for x, b in product(range(1, 10), range(-1, 4)))
+        ]
+
+        for r in results:
+            r.get()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--embeddings', help="path to word embeddings")
+    parser.add_argument('--results_dir', help="path to the results directory", default="results/affix_search")
     args = parser.parse_args()
-    main(args)
+    main(args.results_dir, args.embeddings)
