09451abc1372e3db566d8b93dccbc069db2db00a
Shawn Zhong <github@shawnzhong.com>
2020-05-25 09:20:49 -0500

    Create pos_tune_reg_plot.py

---
+++ pos_tune_reg_plot.py
@@ -0,0 +1,28 @@
+"""
+Script used to plot C vs score
+"""
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--results_dir', help="path to the results directory", default="results/pos_reg_search")
+    args = parser.parse_args()
+
+    xs = []
+    ys = []
+
+    for path in Path(args.results_dir).iterdir():
+        with open(path, 'r') as f:
+            line = f.readline()
+            if len(line) == 0:
+                continue
+            _, score_str = line.split(":")
+            xs.append(float(path.name))
+            ys.append(float(score_str.strip()))
+
+    plt.plot(xs, ys, 'o')
+    plt.xscale('log', basex=10)
+    plt.show()
