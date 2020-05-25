fd92e4ae412a1fe43bb0fd71eef86459b31ef156
Shawn Zhong <github@shawnzhong.com>
2020-05-25 09:38:30 -0500

    Update pos_tune_reg_plot.py

--- pos_tune_reg_plot.py
+++ pos_tune_reg_plot.py
@@ -6,23 +6,25 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--results_dir', help="path to the results directory", default="results/pos_reg_search")
-    args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--results_dir', help="path to the results directory", default="results/pos_reg_search")
+args = parser.parse_args()
 
-    xs = []
-    ys = []
+xs = []
+ys = []
 
-    for path in Path(args.results_dir).iterdir():
-        with open(path, 'r') as f:
-            line = f.readline()
-            if len(line) == 0:
-                continue
-            _, score_str = line.split(":")
-            xs.append(float(path.name))
-            ys.append(float(score_str.strip()))
+for path in Path(args.results_dir).iterdir():
+    with open(path, 'r') as f:
+        line = f.readline()
+        if len(line) == 0:
+            continue
+        _, score_str = line.split(":")
+        xs.append(float(path.name))
+        ys.append(float(score_str.strip()))
 
-    plt.plot(xs, ys, 'o')
-    plt.xscale('log', basex=10)
-    plt.show()
+plt.plot(xs, ys, 'o')
+plt.xscale('log', basex=10)
+plt.show()
+
+for x, y in sorted(zip(xs, ys)):
+    print(f"{x}: {y:.6f}")
