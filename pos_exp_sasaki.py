93719af73d4507541d926a514e72c40b60f27c54
Shawn Zhong <6421097+ShawnZhong@users.noreply.github.com>
2020-05-25 11:33:45 -0500

    POS: Tune regularization for KQV (#12)
    * Update affix_tune_reg.py
    
    * Add args
    
    * Update tune_regularization.py
    
    * Revert "Update tune_regularization.py"
    
    This reverts commit 35e5554087c12479570861283dd713a956e7bd88.
    
    * Update affix_tune_reg.py
    
    * Create pos_tune_reg.py
    
    * Create pos_tune_reg_plot.py
    
    * Update affix_tune_reg.py
    
    * Update pos_tune_reg.py
    
    * Update pos_tune_reg_plot.py
    
    * Update affix_tune_reg.py
    
    * Update pos_tune_reg_plot.py
    
    * Update sasaki_utils.py
    
    * Update sasaki_pos.py
    
    * Update sasaki_pos.py
    
    * Update pos_exp_sasaki.py

--- pos_exp_sasaki.py
+++ pos_exp_sasaki.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 from pathlib import Path
 
 from datasets.polyglot_emb import get_polyglot_codecs_path, prepare_polyglot_emb_paths, languages
-from datasets.polyglot_freq import get_polyglot_frequency_path
+from datasets.polyglot_freq import prepare_polyglot_freq_paths
 from datasets.ud import prepare_ud_paths
 from sasaki_utils import inference, train, get_latest_in_dir, evaluate_pos
 
@@ -11,19 +11,19 @@ def exp(lang):
     epoch = 300
 
     emb_path = prepare_polyglot_emb_paths(lang).w2v_path
-    freq_path = get_polyglot_frequency_path(lang).raw_count_path
+    freq_path = prepare_polyglot_freq_paths(lang).raw_count_path
     codecs_path = get_polyglot_codecs_path(lang)
     ud_data_path, ud_vocab_path = prepare_ud_paths(lang)
 
     result_path = Path(".") / "results" / "sasaki" / "polyglot_KVQ_F" / lang
-    train(emb_path, result_path, freq_path, codecs_path, epoch=epoch, H=40_000, F=500_000)
+    # train(emb_path, result_path, freq_path, codecs_path, epoch=epoch, H=40_000, F=500_000)
 
     result_path = get_latest_in_dir(result_path / "sep_kvq")
     model_path = result_path / f"model_epoch_{epoch}"
-    inference(model_path, codecs_path, ud_vocab_path)
+    # inference(model_path, codecs_path, ud_vocab_path)
 
     ud_vocab_embedding_path = result_path / f"inference_embedding_epoch{epoch}" / "embedding.txt"
-    score = evaluate_pos(ud_data_path, ud_vocab_embedding_path)
+    score = evaluate_pos(ud_data_path, ud_vocab_embedding_path, C=70)
 
     with open(result_path / "score.txt", "w") as f:
         print(f"score for {lang} = {score}", file=f)
