fe82172369c343048562a6cafa50ad2597b01865
Shawn Zhong <github@shawnzhong.com>
2020-05-29 01:32:22 -0500

    tmp save

--- pos_tune_reg.py
+++ pos_tune_reg.py
@@ -36,8 +36,16 @@ def main(results_dir, embeddings, dataset):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--embeddings', required=True, help="path to word embeddings")
     parser.add_argument('--dataset', required=True, help="path to dataset")
-    parser.add_argument('--results_dir', help="path to the results directory", default="results/pos_reg_search")
+    parser.add_argument(
+        '--embeddings',
+        required=True,
+        help="path to word embeddings"
+    )
+    parser.add_argument(
+        '--results_dir',
+        help="path to the results directory",
+        default="results/pos_reg_search"
+    )
     args = parser.parse_args()
     main(args.results_dir, args.embeddings, args.dataset)
