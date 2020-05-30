656cf65734c0dbbacf113d606b5f50e1a1009889
Shawn Zhong <github@shawnzhong.com>
2020-05-30 05:25:37 -0500

    Update ws_eval_target.py

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -1,3 +1,6 @@
+"""
+Used to generate target vector statistics
+"""
 from datasets import prepare_en_target_vector_paths
 from datasets.ws_bench import BENCHS, prepare_bench_paths
 from ws_eval import eval_ws
@@ -10,6 +13,6 @@ for target_vector_name in target_vector_names:
         target_vector_path = prepare_en_target_vector_paths(target_vector_name).txt_emb_path
     for dataset in BENCHS:
         data_path = prepare_bench_paths(dataset).txt_path
-        for lower in (True, False):
-            result = eval_ws(target_vector_path, data_path, lower=lower, oov_handling="zero")
+        for oov_handling in ("drop", "zero"):
+            result = eval_ws(target_vector_path, data_path, lower=True, oov_handling=oov_handling)
             print(target_vector_name, result)
