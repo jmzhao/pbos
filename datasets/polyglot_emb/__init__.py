110c0b2d2e39f9c49dad5134fec65fc0f1891546
Shawn Zhong <github@shawnzhong.com>
2020-05-30 05:56:57 -0500

    dataset changes

--- datasets/polyglot_emb/__init__.py
+++ datasets/polyglot_emb/__init__.py
@@ -1,10 +1,12 @@
-import json
 import logging
 import os
 import shutil
 import subprocess as sp
 import tarfile
 
+import numpy as np
+
+from datasets.utils import save_emb, save_words, is_normal
 from load import load_embedding
 from utils import dotdict
 
@@ -34,33 +36,48 @@ def prepare_polyglot_emb_paths(language_code, *, dir_path=dir_path):
             src_file = tar.extractfile("./words_embeddings_32.pkl")
             shutil.copyfileobj(src_file, dst_file)
 
-    if not os.path.exists(word_freq_path):
-        vocab, emb = load_embedding(pkl_path)
-        with open(word_freq_path, "w") as fout:
-            for word in vocab:
-                print(json.dumps((word, 1)), file=fout)
-
-    if not os.path.exists(w2v_path):
-        vocab, emb = load_embedding(pkl_path)
-        with open(w2v_path, "w") as fout:
-            print(len(vocab), len(emb[0]), file=fout)
-            for v, e in zip(vocab, emb):
-                print(v, *e, file=fout)
-
-    if not os.path.exists(txt_emb_path):
-        vocab, emb = load_embedding(pkl_path)
-        with open(txt_emb_path, "w") as fout:
-            for v, e in zip(vocab, emb):
-                print(v, *e, file=fout)
+    vocab, emb = load_embedding(pkl_path)
+    save_emb(vocab, emb, w2v_emb_path=w2v_path, txt_emb_path=txt_emb_path)
+    save_words(vocab, word_freq_path=word_freq_path)
 
     return dotdict(
-        dir_path = dir_path,
-        language_dir_path = language_dir_path,
-        tar_path = tar_path,
-        pkl_path = pkl_path,
-        word_freq_path = word_freq_path,
-        w2v_path = w2v_path,
-        txt_emb_path = txt_emb_path
+        dir_path=dir_path,
+        language_dir_path=language_dir_path,
+        tar_path=tar_path,
+        pkl_path=pkl_path,
+        word_freq_path=word_freq_path,
+        w2v_path=w2v_path,
+        txt_emb_path=txt_emb_path
+    )
+
+
+def _prepare_polyglot_normalized_en_paths(dir_path=dir_path):
+    language_dir_path = os.path.join(dir_path, "en")
+    raw_en_emb_paths = prepare_polyglot_emb_paths("en", dir_path=dir_path)
+    pkl_path = os.path.join(language_dir_path, "normalized_embeddings.pkl")
+    w2v_path = os.path.join(language_dir_path, "normalized_embeddings.w2v")
+    word_freq_path = os.path.join(language_dir_path, "normalized_word_freq.jsonl")
+    txt_emb_path = os.path.join(language_dir_path, "normalized_embeddings.txt")
+
+    raw_vocab, raw_emb = load_embedding(raw_en_emb_paths.pkl_path)
+    vocab, emb = [], []
+
+    for w, e in (raw_vocab, raw_emb):
+        if is_normal(w):
+            vocab.append(w)
+            emb.append(e)
+    emb = np.array(emb)
+
+    save_emb(vocab, emb, w2v_emb_path=w2v_path, txt_emb_path=txt_emb_path, pkl_emb_path=pkl_path)
+    save_words(vocab, word_freq_path=word_freq_path)
+
+    return dotdict(
+        dir_path=dir_path,
+        language_dir_path=language_dir_path,
+        pkl_path=pkl_path,
+        word_freq_path=word_freq_path,
+        w2v_path=w2v_path,
+        txt_emb_path=txt_emb_path
     )
 
 
@@ -69,7 +86,8 @@ languages = [
     'id', 'it', 'kk', 'lv', 'ro', 'ru', 'sv', 'ta', 'tr', 'vi', 'zh',
 ]
 
-
 if __name__ == '__main__':
     for language_code in languages:
         prepare_polyglot_emb_paths(language_code)
+
+    _prepare_polyglot_normalized_en_paths()
