c8f1f294b703122324de7ed3cb04370b69838c36
Shawn Zhong <github@shawnzhong.com>
2020-05-30 05:22:38 -0500

    Update ws_eval_target.py

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -1,19 +1,15 @@
-from datasets.glove import prepare_glove_paths
+from datasets import prepare_en_target_vector_paths
 from datasets.ws_bench import BENCHS, prepare_bench_paths
 from ws_eval import eval_ws
 
-from datasets.google import prepare_google_paths
-from datasets.polyglot_emb import prepare_polyglot_emb_paths
-
-models = {
-    "EditSim": "EditSim",
-    "ployglot": prepare_polyglot_emb_paths("en").txt_emb_path,
-    "google": prepare_google_paths().txt_emb_path,
-    "glove": prepare_glove_paths().txt_emb_path,
-}
-
-for model_name, model_path in models.items():
+target_vector_names = ("EditSim", "polyglot", "google", "glove")
+for target_vector_name in target_vector_names:
+    if target_vector_name.lower() == "editsim":
+        target_vector_path = "EditSim"
+    else:
+        target_vector_path = prepare_en_target_vector_paths(target_vector_name).txt_emb_path
     for dataset in BENCHS:
         data_path = prepare_bench_paths(dataset).txt_path
-        result = eval_ws(model_path, data_path, lower=True, oov_handling="zero")
-        print(model_name, result)
+        for lower in (True, False):
+            result = eval_ws(target_vector_path, data_path, lower=lower, oov_handling="zero")
+            print(target_vector_name, result)
