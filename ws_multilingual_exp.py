2ee71e4b532fc75970ecb7b191ce07e3ffc9ee8a
Shawn Zhong <github@shawnzhong.com>
2020-08-12 13:58:33 -0500

    Update multi script

--- ws_multilingual_exp.py
+++ ws_multilingual_exp.py
@@ -11,16 +11,13 @@ from ws_eval import eval_ws
 
 def exp(model_type, lang, bname):
     query_path = prepare_bench_paths(bname).query_path
-    result_path = Path(f"results/ws_multi/{bname}/{model_type}")
-    result_path.mkdir(exist_ok=True, parents=True)
-    eval_result_path = result_path / "result.txt"
 
     if model_type == "sasaki":
         from sasaki_utils import inference, get_info_from_result_path
         model_info = get_info_from_result_path(Path(f"results/pos/{lang}/sasaki/sep_kvq"))
         result_emb_path = inference(model_info, query_path)
     else:
-        result_emb_path = result_path / "emb.txt"
+        result_emb_path = f"results/ws_multi/{bname}/{model_type}/emb.txt"
         predict(
             model=f"results/pos/{lang}/{model_type}/model.pbos",
             queries=query_path,
@@ -28,18 +25,18 @@ def exp(model_type, lang, bname):
             word_boundary=(model_type == "bos"),
         )
 
-    with open(eval_result_path, "w") as fout:
-        bench_path = prepare_bench_paths(bname).txt_path
+    bench_path = prepare_bench_paths(bname).txt_path
+    with open("results/ws_multi/result.txt", "a") as fout:
         for lower in (True, False):
-            print(eval_ws(result_emb_path, bench_path, lower=lower, oov_handling='zero'), file=fout)
+            print(model_type.ljust(10), eval_ws(result_emb_path, bench_path, lower=lower, oov_handling='zero'), file=fout)
 
 
 def main():
     model_types = ("pbos", "bos", "sasaki")
 
     for lang in ("en", "it", "ru"):
-        for model_type in model_types:
-            for bname in get_all_bnames_for_lang(lang):
+        for bname in get_all_bnames_for_lang(lang):
+            for model_type in model_types:
                 exp(model_type, lang, bname)
 
 
