9d5ee8696e083c43ccc26e4114f1c82974c88dd4
Shawn Zhong <github@shawnzhong.com>
2020-09-18 09:57:43 -0500

    Update ws eval target script

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -2,7 +2,7 @@
 Used to generate target vector statistics
 """
 from datasets import prepare_target_vector_paths
-from datasets.ws_bench import BENCHS, prepare_bench_paths
+from datasets.ws_bench import BENCHS, prepare_bench_paths, get_all_bnames_for_lang
 from ws_eval import eval_ws
 
 # target_vector_names = ("EditSim", "polyglot", "google", "glove")
@@ -12,7 +12,7 @@ for target_vector_name in target_vector_names:
         target_vector_path = "EditSim"
     else:
         target_vector_path = prepare_target_vector_paths(target_vector_name).txt_emb_path
-    for dataset in BENCHS:
+    for dataset in get_all_bnames_for_lang("en"):
         data_path = prepare_bench_paths(dataset).txt_path
         for oov_handling in ("drop", "zero"):
             result = eval_ws(target_vector_path, data_path, lower=True, oov_handling=oov_handling)
