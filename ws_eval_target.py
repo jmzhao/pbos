76b084a823e25ec7b223e413dd115d06fb83ff9c
Shawn Zhong <github@shawnzhong.com>
2020-09-24 13:41:40 -0500

    Update eval target script

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -5,15 +5,9 @@ from datasets import prepare_target_vector_paths
 from datasets.ws_bench import BENCHS, prepare_bench_paths, get_all_bnames_for_lang
 from ws_eval import eval_ws
 
-# target_vector_names = ("EditSim", "polyglot", "google", "glove")
-target_vector_names = ("polyglot", "google")
-for target_vector_name in target_vector_names:
-    if target_vector_name.lower() == "editsim":
-        target_vector_path = "EditSim"
-    else:
-        target_vector_path = prepare_target_vector_paths(target_vector_name).txt_emb_path
-    for dataset in get_all_bnames_for_lang("en"):
+for lang in ("en", "it", "ru", "de"):
+    target_vector_path = prepare_target_vector_paths(f"wiki2vec-{lang}").txt_emb_path
+    for dataset in get_all_bnames_for_lang(lang):
         data_path = prepare_bench_paths(dataset).txt_path
         for oov_handling in ("drop", "zero"):
-            result = eval_ws(target_vector_path, data_path, lower=True, oov_handling=oov_handling)
-            print(target_vector_name, result)
+            print(eval_ws(target_vector_path, data_path, lower=True, oov_handling=oov_handling))
