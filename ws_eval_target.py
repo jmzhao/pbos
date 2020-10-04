4c18deac977d4c7e62bb9a0b3e1ef648c7a25c34
Shawn Zhong <github@shawnzhong.com>
2020-10-04 17:06:36 -0500

    update eval_target.py

--- ws_eval_target.py
+++ ws_eval_target.py
@@ -5,8 +5,8 @@ from datasets import prepare_target_vector_paths
 from datasets.ws_bench import BENCHS, prepare_bench_paths, get_all_bnames_for_lang
 from ws_eval import eval_ws
 
-for lang in ("en", "it", "ru", "de"):
-    target_vector_path = prepare_target_vector_paths(f"wiki2vec-{lang}").txt_emb_path
+for lang in ("de", "en", "it", "ru",):
+    target_vector_path = prepare_target_vector_paths(f"polyglot-{lang}").txt_emb_path
     for dataset in get_all_bnames_for_lang(lang):
         data_path = prepare_bench_paths(dataset).txt_path
         for oov_handling in ("drop", "zero"):
