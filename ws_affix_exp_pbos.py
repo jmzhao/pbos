7e8e55acdf3ce7f9799f9bee2a67a2e6aa35b95d
Shawn Zhong <github@shawnzhong.com>
2020-10-09 17:09:54 +0800

    minor update

--- ws_affix_exp_pbos.py
+++ ws_affix_exp_pbos.py
@@ -6,9 +6,9 @@ from collections import ChainMap
 
 import pbos_train
 import subwords
-from datasets import prepare_combined_query_path, prepare_target_vector_paths
-from datasets.polyglot_freq import prepare_polyglot_freq_paths
-from datasets.ws_bench import prepare_bench_paths, get_all_bnames_for_lang, prepare_combined_query_path_for_lang
+from datasets import prepare_combined_query_path, prepare_en_target_vector_paths
+from datasets.unigram_freq import prepare_unigram_freq_paths
+from datasets.ws_bench import prepare_bench_paths, BENCHS
 from pbos_pred import predict
 from utils import dotdict
 from utils.args import dump_args
@@ -28,23 +28,22 @@ def train(args):
     pbos_train.main(args)
 
 
-def evaluate(args):
+def evaluate_ws_affix(args):
     with open(args.eval_result_path, "w") as fout:
-        for bname in get_all_bnames_for_lang(args.target_vector_name):
+        for bname in BENCHS:
             bench_path = prepare_bench_paths(bname).txt_path
             for lower in (True, False):
-                print(args.model_type.ljust(10), eval_ws(args.pred_path, bench_path, lower=lower, oov_handling='zero'), file=fout)
+                print(eval_ws(args.pred_path, bench_path, lower=lower, oov_handling='zero'), file=fout)
 
 
 def exp(model_type, target_vector_name):
-    target_vector_paths = prepare_target_vector_paths(f"wiki2vec-{target_vector_name}")
+    target_vector_paths = prepare_en_target_vector_paths(target_vector_name)
     args = dotdict()
 
     # misc
-    args.results_dir = f"results/ws_multi/{target_vector_name}_{model_type}"
+    args.results_dir = f"results/ws_affix/{target_vector_name}_{model_type}"
     args.model_type = model_type
     args.log_level = "INFO"
-    args.target_vector_name = target_vector_name
 
     # subword
     if model_type == "bos":
@@ -71,7 +70,7 @@ def exp(model_type, target_vector_name):
         args.subword_prob = None
     elif model_type in ('pbos', 'pbosn'):
         args.subword_prob_min_prob = 0
-        args.subword_prob_word_freq = prepare_polyglot_freq_paths(target_vector_name).word_freq_path
+        args.subword_prob_word_freq = prepare_unigram_freq_paths().word_freq_path
         args.subword_prob = f"{args.results_dir}/subword_prob.jsonl"
 
     # training
@@ -89,8 +88,9 @@ def exp(model_type, target_vector_name):
         args.normalize_semb = False
 
     # prediction & evaluation
-    args.eval_result_path = f"{args.results_dir}/result.txt"
     args.pred_path = f"{args.results_dir}/vectors.txt"
+    args.query_path = prepare_combined_query_path()
+    args.eval_result_path = f"{args.results_dir}/result.txt"
     os.makedirs(args.results_dir, exist_ok=True)
 
     # redirect log output
@@ -101,31 +101,31 @@ def exp(model_type, target_vector_name):
     with contextlib.redirect_stdout(log_file), contextlib.redirect_stderr(log_file):
         train(args)
 
-        combined_query_path = prepare_combined_query_path_for_lang(args.target_vector_name)
-        
-        predict(
+        # prediction
+        time_used = predict(
             model=args.model_path,
-            queries=combined_query_path,
+            queries=args.query_path,
             save=args.pred_path,
             word_boundary=args.word_boundary,
         )
+        print(f"time used: {time_used:.3f}")
 
-        evaluate(args)
+        # evaluate
+        evaluate_ws_affix(args)
 
 
 if __name__ == '__main__':
-    model_types = ("bos", "pbos")
-    target_vector_names = ("en", "de", "it", "ru", )
+    model_types = ("pbos", "bos")
+    target_vector_names = ("google", "polyglot")
 
     for target_vector_name in target_vector_names:  # avoid race condition
-        prepare_target_vector_paths(f"wiki2vec-{target_vector_name}")
-        prepare_polyglot_freq_paths(target_vector_name)
+        prepare_en_target_vector_paths(target_vector_name)
 
     with mp.Pool() as pool:
         results = [
             pool.apply_async(exp, (model_type, target_vector_name))
-            for target_vector_name in target_vector_names
             for model_type in model_types
+            for target_vector_name in target_vector_names
         ]
 
         for r in results:
--- ws_multilingual_exp_pbos.py
+++ ws_multilingual_exp_pbos.py
@@ -6,9 +6,9 @@ from collections import ChainMap
 
 import pbos_train
 import subwords
-from datasets import prepare_combined_query_path, prepare_en_target_vector_paths
-from datasets.unigram_freq import prepare_unigram_freq_paths
-from datasets.ws_bench import prepare_bench_paths, BENCHS
+from datasets import prepare_combined_query_path, prepare_target_vector_paths
+from datasets.polyglot_freq import prepare_polyglot_freq_paths
+from datasets.ws_bench import prepare_bench_paths, get_all_bnames_for_lang, prepare_combined_query_path_for_lang
 from pbos_pred import predict
 from utils import dotdict
 from utils.args import dump_args
@@ -28,22 +28,23 @@ def train(args):
     pbos_train.main(args)
 
 
-def evaluate_ws_affix(args):
+def evaluate(args):
     with open(args.eval_result_path, "w") as fout:
-        for bname in BENCHS:
+        for bname in get_all_bnames_for_lang(args.target_vector_name):
             bench_path = prepare_bench_paths(bname).txt_path
             for lower in (True, False):
-                print(eval_ws(args.pred_path, bench_path, lower=lower, oov_handling='zero'), file=fout)
+                print(args.model_type.ljust(10), eval_ws(args.pred_path, bench_path, lower=lower, oov_handling='zero'), file=fout)
 
 
 def exp(model_type, target_vector_name):
-    target_vector_paths = prepare_en_target_vector_paths(target_vector_name)
+    target_vector_paths = prepare_target_vector_paths(f"wiki2vec-{target_vector_name}")
     args = dotdict()
 
     # misc
-    args.results_dir = f"results/ws_affix/{target_vector_name}_{model_type}"
+    args.results_dir = f"results/ws_multi/{target_vector_name}_{model_type}"
     args.model_type = model_type
     args.log_level = "INFO"
+    args.target_vector_name = target_vector_name
 
     # subword
     if model_type == "bos":
@@ -70,7 +71,7 @@ def exp(model_type, target_vector_name):
         args.subword_prob = None
     elif model_type in ('pbos', 'pbosn'):
         args.subword_prob_min_prob = 0
-        args.subword_prob_word_freq = prepare_unigram_freq_paths().word_freq_path
+        args.subword_prob_word_freq = prepare_polyglot_freq_paths(target_vector_name).word_freq_path
         args.subword_prob = f"{args.results_dir}/subword_prob.jsonl"
 
     # training
@@ -88,9 +89,8 @@ def exp(model_type, target_vector_name):
         args.normalize_semb = False
 
     # prediction & evaluation
-    args.pred_path = f"{args.results_dir}/vectors.txt"
-    args.query_path = prepare_combined_query_path()
     args.eval_result_path = f"{args.results_dir}/result.txt"
+    args.pred_path = f"{args.results_dir}/vectors.txt"
     os.makedirs(args.results_dir, exist_ok=True)
 
     # redirect log output
@@ -101,31 +101,31 @@ def exp(model_type, target_vector_name):
     with contextlib.redirect_stdout(log_file), contextlib.redirect_stderr(log_file):
         train(args)
 
-        # prediction
-        time_used = predict(
+        combined_query_path = prepare_combined_query_path_for_lang(args.target_vector_name)
+        
+        predict(
             model=args.model_path,
-            queries=args.query_path,
+            queries=combined_query_path,
             save=args.pred_path,
             word_boundary=args.word_boundary,
         )
-        print(f"time used: {time_used:.3f}")
 
-        # evaluate
-        evaluate_ws_affix(args)
+        evaluate(args)
 
 
 if __name__ == '__main__':
-    model_types = ("pbos", "bos")
-    target_vector_names = ("google", "polyglot")
+    model_types = ("bos", "pbos")
+    target_vector_names = ("en", "de", "it", "ru", )
 
     for target_vector_name in target_vector_names:  # avoid race condition
-        prepare_en_target_vector_paths(target_vector_name)
+        prepare_target_vector_paths(f"wiki2vec-{target_vector_name}")
+        prepare_polyglot_freq_paths(target_vector_name)
 
     with mp.Pool() as pool:
         results = [
             pool.apply_async(exp, (model_type, target_vector_name))
-            for model_type in model_types
             for target_vector_name in target_vector_names
+            for model_type in model_types
         ]
 
         for r in results:
